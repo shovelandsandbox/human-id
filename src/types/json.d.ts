dc5a0b6690c6dfa8af95329e38788f747ef1f676
Joey Figaro <joey@joeyfigaro.com>
2020-02-15 16:59:30 -0500

    added json module support

---
+++ src/types/json.d.ts
@@ -0,0 +1,24 @@
+// declare module 'adjectives.json' {
+//   var adjectives: any;
+//   export default adjectives;
+// }
+
+// declare module 'animals.json' {
+//   var animals: any;
+//   export default animals;
+// }
+
+// declare module 'color-attributes.json' {
+//   var colorAttributes: any;
+//   export default colorAttributes;
+// }
+
+// declare module 'colors.json' {
+//   var colors: any;
+//   export default colors;
+// }
+
+declare module '*.json' {
+  var data: []
+  export default data
+}
